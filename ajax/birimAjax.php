21e440e2459e103d41bc30e5038c43fecda6846a
zekikus <zekiikuss@hotmail.com>
2016-06-12 09:20:21 +0300

    Birim Tanıma Bildiirim Yapılacak Kullanıcı Eklendi

--- ajax/birimAjax.php
+++ ajax/birimAjax.php
@@ -41,6 +41,8 @@
 				$(document).ready(function(){
 					$('#stokLink').show();
     				$('#kullaniciLink').show();
+    				$('#kullanici1 select').val('0');
+					$('#kullanici2 select').val('0');
 					$('#birimID').val('".$satir['id']."');
 					$('#ad').val('".$satir['ad']."');
 					$('#birimIl select').prop('disabled','disabled');
@@ -53,6 +55,29 @@
 				});
 			</script>";
 		}
+
+		$sorgu = "SELECT b.*,k.id as 'kullaniciID' FROM birim b,kullanici k WHERE b.id = k.birimID and b.id = $deger";
+		$sonuc = $kontrol -> listele($sorgu);
+		$etkilenenKayitlar = $kontrol -> etkilenenKayitSayisi($sorgu);
+
+		if($etkilenenKayitlar > 0){
+			$sayac = 1;
+			while ($satir = mysqli_fetch_assoc($sonuc)) {
+				echo "<script>
+				$(document).ready(function(){
+					$('#kullanici".$sayac." select').val('".$satir['kullaniciID']."');
+				});
+				</script>";
+			$sayac++;
+			}
+		}else{
+			echo "<script>
+				$(document).ready(function(){
+					$('#kullanici1 #firstOpt,#kullanici2 #firstOpt').attr('selected','true');
+				});
+				</script>";
+		}
+		
 		setcookie("birimID",$deger,time() + 30);
 	}
 
@@ -61,6 +86,8 @@
 		$birimIl = $query["birimIl"];
 		$birimIlce = $query["birimIlce"];
 		$birimAdres = $query["birimAdres"];
+		$kullaniciID = $query["kullaniciID"];
+		$kullaniciID2 = $query["kullaniciID2"];
 
 		$id = $_COOKIE['birimID'];
 		
@@ -68,8 +95,12 @@
 		$kontrol = new BirimKontrol();
 		$kontrol -> duzenle($sorgu,"guncelle");
 
+		birimKullaniciKontrol($id,$kullaniciID,$kullaniciID2);
+
 		echo "<script>
 			$(document).ready(function(){
+				$('#kullanici1 select').val('0');
+				$('#kullanici2 select').val('0');
 				$('#kaydetBT').val('');
 				$('#kaydetBT').html('Kaydet');
 			});
@@ -84,13 +115,21 @@
 		$birimIl = $query["birimIl"];
 		$birimIlce = $query["birimIlce"];
 		$birimAdres = $query["birimAdres"];
+		$kullaniciID = $query["kullaniciID"];
+		$kullaniciID2 = $query["kullaniciID2"];
 
 		$sorgu = "INSERT INTO `birim` (`id`, `ad`, `il`, `ilce`, `adres`) VALUES (NULL, '".$ad."', '".$birimIl."', '".$birimIlce."', '".$birimAdres."')";
 
 		$kontrol = new BirimKontrol();
 		$kontrol -> kaydet($sorgu);
 
-		kayitListele($birimIl,$birimIlce);
+		$sonuc = $kontrol -> listele("SELECT id FROM birim ORDER BY id DESC LIMIT 0,1");
+
+		$son = mysqli_fetch_assoc($sonuc);
+
+		birimKullaniciKontrol($son['id'],$kullaniciID,$kullaniciID2);
+
+		kayitListele($birimIl,$birimIlce);	
 	}
 
 	function kayitSil($deger){
@@ -115,6 +154,15 @@
 						echo "<option value=".$satir["id"].">".$satir["ad"]."</option>";
 			  	}
 			echo "</select><a onclick=\"ajaxCokluListele('birimAjax');\"><span class='glyphicon glyphicon-list'></span></a><br/>";
+	}
+
+	function birimKullaniciKontrol($id,$kullaniciID,$kullaniciID2){
+		$kontrol = new BirimKontrol();
+
+		$kontrol -> sorguCalistir("UPDATE kullanici SET birimID = ".$id." WHERE id = $kullaniciID or id = $kullaniciID2"
+			,"guncelle");
+
+		$kontrol -> sorguCalistir("UPDATE kullanici SET birimID = 0 WHERE birimID = ".$id." and id <> $kullaniciID and id <> $kullaniciID2","guncelle");
 	}
 
 	function formGetir($query){
